5652aa7b99056b88532cb333ff3e6278c1e4e8cf
João Victor <victtorjoaorslv@gmail.com>
2021-01-12 20:29:46 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:login_page/login.page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'balta.io',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: LoginPage(),
+    );
+  }
+}
